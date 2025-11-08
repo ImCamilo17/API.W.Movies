2adef3add81766f6d014e43db502fdc9470af822
Camil <Camil@DESKTOP-FSBM14Q>
2025-11-08 15:24:54 -0500

    Add project files.

---
+++ API.W.Movies/DAL/Models/AuditBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace API.W.Movies.DAL.Models
+{
+    public class AuditBase
+    {
+        [Key] //Indica que el campo es clave primaria
+        public virtual int Id { get; set; }
+        public virtual int CreteDate { get; set; }
+        public virtual int ModifiedDate { get; set; }
+    }
+}
---
+++ API.W.Movies/DAL/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.W.Movies.DAL.Models
+{
+    public class Category: AuditBase
+    {
+        [Required] //indica que el campo es obligatorio
+        public string Name { get; set; }
+    
+    }
+}
